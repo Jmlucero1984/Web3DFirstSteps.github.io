81a73fbe8d39af121e88bece8d0468eb68715e18
Jmlucero1984 <jmlucero1984@gmail.com>
2023-04-19 16:12:25 -0300

    Change to Example of Three

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
-// Creamos la escena, la cámara y el renderizador
+import * as THREE from 'three';
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
 const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-// Configuramos el renderizador
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-// Creamos el plano sobre el que colocaremos el modelo
-const geometry = new THREE.PlaneGeometry(100, 100, 10, 10);
-const material = new THREE.MeshBasicMaterial({color: 0xaaaaaa, wireframe: true});
-const plane = new THREE.Mesh(geometry, material);
-plane.rotation.x = -Math.PI / 2;
-scene.add(plane);
-
-// Movemos la cámara hacia atrás para que veamos el modelo
 camera.position.z = 5;
 
-// Creamos los controles para mover el modelo
-const controls = new THREE.TrackballControls(camera, renderer.domElement);
-controls.rotateSpeed = 5;
-controls.zoomSpeed = 1.2;
-controls.panSpeed = 0.8;
-controls.noZoom = false;
-controls.noPan = false;
-controls.staticMoving = true;
-controls.dynamicDampingFactor
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
+animate();
